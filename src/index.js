16c086da164c12abfd03f3bf9e79596fb9c21d88
tkoenig97 <tylerskoenig@gmail.com>
2022-10-27 12:35:45 -0500

    Moved geolocation call to constructor

--- src/index.js
+++ src/index.js
@@ -2,13 +2,24 @@ import React from 'react' ;
 import ReactDom from 'react-dom';
 
 class App extends React.Component {
-    render() {
+
+    constructor(props) {
+        super(props);
+
+        this.state = { lat: null };
+
         window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
+            (position) => {
+                // We called setState!!
+                this.setState({ lat: position.coords.latitude });
+            },
             (err) => console.log(err)
         );
-        
-        return <div>Latitude</div>;
+    }
+
+    // React says we have to define render!!
+    render() {
+        return <div>Latitude: {this.state.lat}</div>;
     }
 }
 
