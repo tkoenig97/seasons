e08a30d62e7ebb7d8eae14524162a1339cac6122
tkoenig97 <tylerskoenig@gmail.com>
2022-10-27 12:52:38 -0500

    Added an error message to state and render method

--- src/index.js
+++ src/index.js
@@ -6,20 +6,32 @@ class App extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = { lat: null };
+        // This is the only time we do direct assignment to this.state
+        this.state = { lat: null, errorMessage: '' };
 
         window.navigator.geolocation.getCurrentPosition(
             (position) => {
                 // We called setState!!
                 this.setState({ lat: position.coords.latitude });
+
+                // We did not:
+                // this.state.lat = position.coords.latitude
             },
-            (err) => console.log(err)
+            err => {
+                this.setState({ errorMessage: err.message });
+            }
         );
     }
 
     // React says we have to define render!!
     render() {
-        return <div>Latitude: {this.state.lat}</div>;
+        return (
+            <div>
+                Latitude: {this.state.lat}
+                <br />
+                Error: {this.state.errorMessage}
+            </div>
+        );
     }
 }
 
