1e389165f3fb8a5e9e42f948cbd586bb3bb9b673
tkoenig97 <tylerskoenig@gmail.com>
2022-10-28 16:13:16 -0500

    Added styling for a loading spinner

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react' ;
 import ReactDom from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './spinner';
 
 class App extends React.Component {
     state = { lat: null, errorMessage: '' };
@@ -23,10 +24,10 @@ class App extends React.Component {
         }
 
         if (!this.state.errorMessage && this.state.lat) {
-            return <SeasonDisplay />
+            return <SeasonDisplay lat={this.state.lat} />
         }
 
-        return <div>Loading...</div>
+        return <Spinner />;
     }
 }
 
