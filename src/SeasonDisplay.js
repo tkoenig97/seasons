0d693b3e9e3bd5a6ecc60991094d4b1e92c862e8
tkoenig97 <tylerskoenig@gmail.com>
2022-10-28 15:52:00 -0500

    Added seasonConfig

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,5 +1,16 @@
 import React from 'react' ;
 
+const seasonConfig = {
+    summer: {
+        text: "Let's hit the beach",
+        iconName: 'sun'
+    },
+    winter: {
+        text: 'Burr, it is cold', 
+        iconName: 'snowflake'
+    }
+};
+
 const getSeason = (lat, month) => {
     if (month > 2 && month < 9) {
         return lat > 0 ? 'summer' : 'winter';
