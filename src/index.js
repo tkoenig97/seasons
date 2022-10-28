aba0bdaab9cb6c7b69f8ddf4016438167f06d425
tkoenig97 <tylerskoenig@gmail.com>
2022-10-28 14:21:51 -0500

    Removed constructor from App class

--- src/index.js
+++ src/index.js
@@ -2,13 +2,7 @@ import React from 'react' ;
 import ReactDom from 'react-dom';
 
 class App extends React.Component {
-
-    constructor(props) {
-        super(props);
-
-        // This is the only time we do direct assignment to this.state
-        this.state = { lat: null, errorMessage: '' };
-    }
+    state = { lat: null, errorMessage: '' };
 
     componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
